bb274508c0a0ed2667fc096b4ed62426f1e5791d
mssjaney <janey.webdev@gmail.com>
2023-10-19 20:41:27 +0200

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,4 +1,28 @@
 const { JSDOM } = require('jsdom');
+const fetch = require('node-fetch');
+
+async function crawlPage(currentURL) {
+    console.log(`Actively crawling: ${currentURL}`);
+
+    try {
+        const resp = await fetch('https://wagslane.dev/');
+
+        if (resp.status > 399) {
+            console.log(`Error in fetch with status code: ${resp.status}, on page: ${currentURL}`);
+            return;
+        }
+
+        const contentType = resp.headers.get("content-type");
+        if (!contentType.includes("text/html")) {
+            console.log(`Error - non HTML response, content type: ${contentType}, on page: ${currentURL}`);
+            return;
+        }
+
+        console.log(await resp.text());
+    } catch (err) {
+        console.log (`Error in fetch: ${err.message}, on page: ${currentURL}`);
+    }
+}
 
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = [];
@@ -41,5 +65,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('hola')
+const { crawlPage } = require('./crawl');
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log('No website provided');
+        process.exit(1);
+    }
+
+    if (process.argv.length > 3) {
+        console.log('Too many command line arguments');
+        process.exit(1);
+    }
+
+    const baseURL = process.argv[2]
+
+    console.log(`starting crawl of ${baseURL}`);
+    crawlPage(baseURL);
+}
+
+main();
